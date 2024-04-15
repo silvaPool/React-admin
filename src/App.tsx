f1c1461cc5a38c1f2df115919dfc96b825d0bc91
silvaPool <silva.friends22@gmail.com>
2024-04-15 10:04:24 -0300

    Trocando ListGuesser pelo novo componente criado

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,10 @@ import {
   ShowGuesser,
 } from "react-admin";
 import { dataProvider } from "./dataProvider";
+import { UserList } from "./Users";
 
 export const App = () => 
  <Admin dataProvider={dataProvider}>
-    <Resource name="users" list={ListGuesser} />
+    {/* <Resource name="users" list={ListGuesser} /> */}
+    <Resource name="users" list={UserList} />
  </Admin>;
