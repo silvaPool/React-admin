2ceb0429be662a1a4d6c928b722b0aaebb41ae2d
silvaPool <silva.friends22@gmail.com>
2024-04-15 11:27:37 -0300

    Usando RefenceField

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,13 @@ import {
 } from "react-admin";
 import { dataProvider } from "./dataProvider";
 import { UserList } from "./Users";
+import { PostList } from "./posts";
 
 export const App = () => 
  <Admin dataProvider={dataProvider}>
     {/* <Resource name="users" list={ListGuesser} /> */}
-    <Resource name="users" list={UserList} />
+    {/* <Resource name="posts" list={ListGuesser} /> */}
+    <Resource name="posts" list={PostList} />
+    {/* <Resource name="users" list={UserList} /> */}
+    <Resource name="users" list={UserList} recordRepresentation="name" />
  </Admin>;
