031616e326d4bfdef3e3303bca073a9b8b6046f7
silvaPool <silva.friends22@gmail.com>
2024-04-15 13:30:48 -0300

    Componente de edição adicionado

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,8 @@ export const App = () =>
  <Admin dataProvider={dataProvider}>
     {/* <Resource name="users" list={ListGuesser} /> */}
     {/* <Resource name="posts" list={ListGuesser} /> */}
-    <Resource name="posts" list={PostList} />
+    {/* <Resource name="posts" list={PostList} /> */}
+    <Resource name="posts" list={PostList} edit={EditGuesser} />
     {/* <Resource name="users" list={UserList} /> */}
     {/* <Resource name="users" list={UserList} recordRepresentation="name" /> */}
     <Resource name="users" list={UserList} show={ShowGuesser} />
