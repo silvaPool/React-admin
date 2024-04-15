eb1533e57f350b5f3a423d37571150b12901d79b
silvaPool <silva.friends22@gmail.com>
2024-04-15 10:03:05 -0300

    Novo componente de página criado

---
+++ src/Users.tsx
@@ -0,0 +1,16 @@
+import { Datagrid, List, TextField } from "react-admin";
+
+export const UserList = () => (
+    <List>
+        <Datagrid rowClick="edit">
+            <TextField source="id" />
+            <TextField source="name" />
+            <TextField source="username" />
+            <TextField source="email" />
+            <TextField source="address.street" />
+            <TextField source="phone" />
+            <TextField source="website" />
+            <TextField source="company.name" />
+        </Datagrid>
+    </List>
+);
