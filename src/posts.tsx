76647d11f0ac5140f2445835822d8e8311d528db
silvaPool <silva.friends22@gmail.com>
2024-04-15 13:40:41 -0300

    Mais alguns ajustes

--- src/posts.tsx
+++ src/posts.tsx
@@ -18,10 +18,12 @@ export const PostList = () => {
 export const PostEdit = () => (
     <Edit>
         <SimpleForm>
+            <TextInput source="id" InputProps={{disabled: true}} />
             <ReferenceInput source="userId" reference="users" />
-            <TextInput source="id" />
+            {/* <TextInput source="id" /> */}
             <TextInput source="title" />
-            <TextInput source="body" />
+            {/* <TextInput source="body" /> */}
+            <TextInput source="body" multiline rows={5} />
         </SimpleForm>
     </Edit>
 )
