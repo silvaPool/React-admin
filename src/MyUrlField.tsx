136e57af620fd8bc5a896691954f95c9f5a43c6a
silvaPool <silva.friends22@gmail.com>
2024-04-15 11:14:30 -0300

    Aplicando Styles no link

--- src/MyUrlField.tsx
+++ src/MyUrlField.tsx
@@ -1,9 +1,17 @@
+import { Link } from "@mui/material";
 import { useRecordContext } from "react-admin"
+import LaunchIcon from "@mui/icons-material/Launch";
 
 const MyUrlField = ({source}: {source: string}) => {
     const record = useRecordContext();
-    if(!record) return null;
-    return <a href={`{http://${record[source]}`}>{record[source]}</a>;
+    return record ? (
+        <Link href={record[source]} sx={{textDecoration: "none"}}>
+            {record[source]}
+            <LaunchIcon sx={{fontSize: 15, ml: 1}} />
+        </Link>
+    ) : null;
+    // if(!record) return null;
+    // return <a href={`{http://${record[source]}`}>{record[source]}</a>;
 };
 
 export default MyUrlField;
