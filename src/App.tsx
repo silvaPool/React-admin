67a9024c8042f3a8c63f104b5e753f3c966e7f00
silvaPool <silva.friends22@gmail.com>
2024-04-15 13:37:45 -0300

    Adicionando PostEdit ao invés do adivinhador

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,16 @@ import {
 } from "react-admin";
 import { dataProvider } from "./dataProvider";
 import { UserList } from "./Users";
-import { PostList } from "./posts";
+import { PostEdit, PostList } from "./Posts";
 
 export const App = () => 
  <Admin dataProvider={dataProvider}>
     {/* <Resource name="users" list={ListGuesser} /> */}
     {/* <Resource name="posts" list={ListGuesser} /> */}
     {/* <Resource name="posts" list={PostList} /> */}
-    <Resource name="posts" list={PostList} edit={EditGuesser} />
+    {/* <Resource name="posts" list={PostList} edit={EditGuesser} /> */}
     {/* <Resource name="users" list={UserList} /> */}
     {/* <Resource name="users" list={UserList} recordRepresentation="name" /> */}
+    <Resource name="posts" list={PostList} edit={PostEdit} />
     <Resource name="users" list={UserList} show={ShowGuesser} />
  </Admin>;
--- src/posts.tsx
+++ src/posts.tsx
@@ -1,14 +1,27 @@
-import { Datagrid, EditButton, List, ReferenceField, TextField } from "react-admin";
+import { Datagrid, Edit, EditButton, List, ReferenceField, ReferenceInput, SimpleForm, TextField, TextInput } from "react-admin";
 
-export const PostList = () => (
-    <List>
-        <Datagrid rowClick="edit">
-        <TextField source="id" />
-            {/* <ReferenceField source="userId" reference="users" /> */}
-            <ReferenceField source="userId" reference="users" link="show" />
-            <TextField source="title" />
-            {/* <TextField source="body" /> */}
-            <EditButton />
-        </Datagrid>
-    </List>
+export const PostList = () => {
+
+    return (
+        <List>
+            <Datagrid rowClick="edit">
+            <TextField source="id" />
+                {/* <ReferenceField source="userId" reference="users" /> */}
+                <ReferenceField source="userId" reference="users" link="show" />
+                <TextField source="title" />
+                {/* <TextField source="body" /> */}
+                <EditButton />
+            </Datagrid>
+        </List>
+    )
+};
+export const PostEdit = () => (
+    <Edit>
+        <SimpleForm>
+            <ReferenceInput source="userId" reference="users" />
+            <TextInput source="id" />
+            <TextInput source="title" />
+            <TextInput source="body" />
+        </SimpleForm>
+    </Edit>
 )
